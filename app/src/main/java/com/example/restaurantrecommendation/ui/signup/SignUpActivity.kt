6c2e4d0aa1d42bdcc88954b7702474362c3260c3
alpiansyah1204 <96229888+alpiansyah1204@users.noreply.github.com>
2022-05-21 02:27:21 +0700

    login and signup

---
+++ app/src/main/java/com/example/restaurantrecommendation/ui/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.restaurantrecommendation.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.restaurantrecommendation.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
