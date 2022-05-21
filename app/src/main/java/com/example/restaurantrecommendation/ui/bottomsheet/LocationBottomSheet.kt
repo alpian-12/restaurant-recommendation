d8ceb3f5d36ce404d596c177662ccfe360a90b25
kartikasw <kartikasariwidagdo@gmail.com>
2022-05-21 09:53:24 +0700

    add location chips

--- app/src/main/java/com/example/restaurantrecommendation/ui/bottomsheet/LocationBottomSheet.kt
+++ app/src/main/java/com/example/restaurantrecommendation/ui/bottomsheet/LocationBottomSheet.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.restaurantrecommendation.R
 import com.example.restaurantrecommendation.databinding.BottomSheetLocationBinding
+import com.example.restaurantrecommendation.model.Location
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
 
 class LocationBottomSheet: BottomSheetDialogFragment() {
     private var _binding: BottomSheetLocationBinding? = null
     private val binding get() = _binding!!
+
+    private val list = ArrayList<Location>()
 
     companion object {
         const val TAG = "LocationBottomSheet"
@@ -23,5 +29,36 @@ class LocationBottomSheet: BottomSheetDialogFragment() {
         _binding = BottomSheetLocationBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        list.addAll(listLocations)
+        getLocation()
+    }
+
+    private val listLocations: ArrayList<Location>
+        get() {
+            val name = resources.getStringArray(R.array.location)
+            val listLocation = ArrayList<Location>()
+            for (i in name.indices) {
+                val location = Location(name[i])
+                listLocation.add(location)
+            }
+            return listLocation
+        }
+
+    private fun getLocation() {
+        for(i in 0 until list.size) {
+            val chip = Chip(context)
+            chip.text = list[i].name
+
+            chip.setOnClickListener {
+                Toast.makeText(context, "Location is chosen", Toast.LENGTH_SHORT).show()
+                this.dismiss()
+            }
+
+            binding.chipGroup.addView(chip)
+        }
     }
 }
