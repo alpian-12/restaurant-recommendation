d17e28ce4bb8b032206cef89e7f496a6dfcb6054
alpiansyah1204 <96229888+alpiansyah1204@users.noreply.github.com>
2022-05-24 21:25:15 +0700

    bottomsheet

--- app/src/main/java/com/example/restaurantrecommendation/adapter/RestaurantAdapter.kt
+++ app/src/main/java/com/example/restaurantrecommendation/adapter/RestaurantAdapter.kt
@@ -1,17 +1,25 @@
 package com.example.restaurantrecommendation.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.restaurantrecommendation.databinding.ItemRestaurantBinding
 import com.example.restaurantrecommendation.model.Restaurant
+import com.example.restaurantrecommendation.ui.detailrestaurant.DetailRestaurantActivity
 
 class RestaurantAdapter(private val restaurants : ArrayList<Restaurant>): RecyclerView.Adapter<RestaurantAdapter.MyViewHolder>() {
+
     inner class MyViewHolder(private val binding: ItemRestaurantBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind (restaurant: Restaurant) {
             binding.apply {
                 with(restaurant) {
                     RestaurantName.text = name
+                }
+                itemView.setOnClickListener {
+                    val intent = Intent(itemView.context, DetailRestaurantActivity::class.java)
+                    itemView.context.startActivity(intent)
                 }
             }
         }
